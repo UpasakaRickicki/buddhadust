c72e7c4e23fe0d632e3f7e7f00818aec85471ae4
Bhante Subhuti <bksubhuti@gmail.com>
2019-08-16 20:42:53 -1000

    These files are for the plugin
    Zip these files in the bhante directory and then add to plugin and then run
    It should do the very first regex

---
+++ pts-ebooks/mn/MyPlugin/bhante/plugin.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+ 
+import sys 
+import re  #for regex
+
+
+text_type = str 
+def run(bk):  
+    for (id, href) in bk.text_iter():  #For each html file in the 'text' section of the epbu   
+        print('Chapter found %s:' % href) #Print the section name   
+        html = bk.readfile(id)   #Read the section into html   
+        if not isinstance(html, text_type): #If the section is not str    
+            html = text_type(html, 'utf-8') #then sets its type to 'utf-8'   
+
+        html_orig = html     #Copy the text to html_orig 
+ 
+        html = re.sub(r'<head>.*Sections<\/a>]<\/p>',r'<head></head><body>', html,0,re.DOTALL) 
+ 
+        if not html == html_orig:   #If the text has changed         
+            bk.writefile(id, html)   #Write the amended text to the book 
+            
+    return 0    # 0 - means success, anything else means failure  
+
+
+def main():     
+    print ("I reached main when I should not have\n")     
+    return -1      
+if __name__ == "__main__":     
+    sys.exit(main()) 
+ 
