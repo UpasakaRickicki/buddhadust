fbed865e5473c9177aa1f1adbf6eaf07de8988ca
Stephen Torrence <captainvalor@gmail.com>
2019-08-18 14:02:09 -0400

    Update copy-an-example.sh

--- pts-ebooks/an/copy-an-example.sh
+++ pts-ebooks/an/copy-an-example.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# make executable using chmod +x FILENAME.sh then run ./FILENAME.sh
+# make executable using `chmod +x FILENAME.sh` then run using `./FILENAME.sh`
 
-#modify these to match your particular file system
+# modify these to match your particular file system
 source_dir=~/buddhadust/dhamma-vinaya/pts/an/
 dest_dir=~/buddhadust/pts-ebooks/an/AN-all/
 
-#rm -rf "$dest_dir/*" #Remove the contents of DESTINATION
+# rm -rf "$dest_dir/*" #Remove the contents of DESTINATION
 cp -r "$source_dir"/01_ones/* "$dest_dir" #Copy the contents of SRC into DESTINATION
 cp -r "$source_dir"/02_twos/* "$dest_dir" #Copy the contents of SRC into DESTINATION
 cp -r "$source_dir"/03_threes/* "$dest_dir" #Copy the contents of SRC into DESTINATION
